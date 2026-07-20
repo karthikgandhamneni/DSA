81f5f7800956e4eb3e57d5cd8449a9a858258403
Karthik Gandhamneni <gvpkarthik12@gmail.com>
2026-07-20 22:21:30 +0530

    .

---
+++ Arrays/3Sum.java
@@ -0,0 +1,25 @@
+class Solution {
+    public List<List<Integer>> threeSum(int[] nums) {
+        Arrays.sort(nums);
+        List<List<Integer>> ans = new ArrayList<>();
+        for(int i=0;i<nums.length;i++){
+            if(i>0&&nums[i]==nums[i-1])
+                continue;
+            List<Integer> list = new ArrayList<>();
+            int l=i+1,r=nums.length-1;
+            while(l<r){
+                int sum= nums[l]+nums[r]+nums[i];
+                if(sum==0){
+                    ans.add(Arrays.asList(nums[i],nums[l],nums[r]));
+                    l++;
+                    r--;
+                    while(l<r && nums[l] ==nums[l-1]) l++;
+                    while(l<r && nums[r]==nums[r+1]) r--;
+                }
+                else if(sum<0) l++;
+                else r--;
+            }
+        }
+        return ans;
+    }
+}
