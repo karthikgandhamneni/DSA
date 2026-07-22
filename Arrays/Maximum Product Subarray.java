0ec2ab1f6b212732e9f15561d574832507166f2f
Karthik Gandhamneni <gvpkarthik12@gmail.com>
2026-07-22 20:51:16 +0530

    solution added

---
+++ Arrays/Maximum Product Subarray.java
@@ -0,0 +1,27 @@
+class Solution {
+    public int maxProduct(int[] nums) {
+
+
+
+
+        int pro=1;
+        int max=Integer.MIN_VALUE;
+        for(int i=0;i<nums.length;i++){
+            pro*=nums[i];
+            max=Math.max(max,pro);
+            if(pro==0) pro=1;
+        }
+        pro=1;
+        for(int i=nums.length-1;i>=0;i--){
+            pro*=nums[i];
+            max=Math.max(max,pro);
+            if(pro==0) pro=1;
+        }
+        return max;
+
+
+        
+        
+       
+    }
+}
