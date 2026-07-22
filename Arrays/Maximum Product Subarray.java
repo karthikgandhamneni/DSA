cf25fcd7a38d0845b7d8b9cfb2a8ce5837b2655b
Karthik Gandhamneni <gvpkarthik12@gmail.com>
2026-07-22 20:52:24 +0530

    solution added

--- Arrays/Maximum Product Subarray.java
+++ Arrays/Maximum Product Subarray.java
@@ -4,7 +4,26 @@ class Solution {
     public int maxProduct(int[] nums) {
 
 
+        // int ans=nums[0],max=nums[0],min=nums[0];
 
+        // for(int i=1;i<nums.length;i++){
+
+        //     int curr = nums[i];
+
+        //     if(curr<0){
+        //         int temp=max;
+        //         max=min;
+        //         min=temp;
+        //     }
+
+        //     max=Math.max(curr,max*curr);
+        //     min=Math.min(curr,min*curr);
+        //     ans=Math.max(max,ans);
+        // }
+        // return ans;
+
+
+        
 
         int pro=1;
         int max=Integer.MIN_VALUE;
