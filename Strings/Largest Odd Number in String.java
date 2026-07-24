9e8b44a3818bb03b05b46cf209ff9bd39543d1d8
Karthik Gandhamneni <gvpkarthik12@gmail.com>
2026-07-24 23:45:49 +0530

    solution addedd

---
+++ Strings/Largest Odd Number in String.java
@@ -0,0 +1,16 @@
+class Solution {
+    public String largestOddNumber(String num) {
+        int ind=-1;
+        for(int i=num.length()-1;i>=0;i--){
+            if((num.charAt(i)-'0')%2==1){
+                ind=i;
+                break;
+            }
+        }
+        int i=0;
+        while(i>ind && num.charAt(i)=='0'){
+            i++;
+        }
+        return num.substring(i,ind+1);
+    }
+}
