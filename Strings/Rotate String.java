e9f22882f24e00895e000769c93aed8fa91f72bc
Karthik Gandhamneni <gvpkarthik12@gmail.com>
2026-07-27 21:45:51 +0530

    solution added

---
+++ Strings/Rotate String.java
@@ -0,0 +1,20 @@
+class Solution {
+    public boolean rotateString(String s, String goal) {
+        int count=0,i;
+        while(true){
+            if(s.equals(goal)){
+                return true;
+            }
+            if(count==s.length()-1)
+                return false;
+            char temp = s.charAt(0);
+            char[] str = s.toCharArray();
+            for(i=0;i<s.length()-1;i++){
+                str[i]=str[i+1];
+            }
+            str[i]=temp;
+            count++;
+            s = new String(str);
+        }
+    }
+}
